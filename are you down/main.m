0c417ea913ab64194d8f68d18c275e49962c0fdd
Robert Crosby <rcros97@me.com>
2017-05-18 20:12:44 -0700

    Initial Commit

---
+++ are you down/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  are you down
+//
+//  Created by Robert Crosby on 5/18/17.
+//  Copyright © 2017 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
